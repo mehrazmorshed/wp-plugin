a4051817d967243866b54706c54d950384842b4d
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-12-05 11:02:13 +0600

    plugin header

--- wp-plugin.php
+++ wp-plugin.php
@@ -1,1 +1,16 @@
 <?php
+/*
+ * Plugin Name:       WP Plugin
+ * Plugin URI:        https://example.com/plugins/wp-plugin/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mehraz Morshed
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       wp-plugin
+ * Domain Path:       /languages
+ */
